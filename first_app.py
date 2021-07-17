526049424ef8e2d09abf95ac24b48a8bf1ea5e6e
nee <nee@hoge.cn>
2021-07-17 11:51:23 +0800

    first commit

---
+++ first_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+
+st.title('Counter Example')
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+increment = st.button('Increment')
+if increment:
+    st.session_state.count += 1
+
+st.write('Count = ', st.session_state.count)
